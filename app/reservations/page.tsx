3a6112a1eb219d70ba81cfb114bb4616dfedc49c
Ivan <106531085+ivannikcevicc@users.noreply.github.com>
2024-04-29 02:11:33 -0700

    Add files via upload

---
+++ app/reservations/page.tsx
@@ -0,0 +1,33 @@
+import { EmptyState } from "../components/EmptyState";
+
+import getCurrentUser from "../actions/getCurrentUser";
+import getReservations from "../actions/getReservations";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser()
+  if(!currentUser ) {
+    return (
+        <EmptyState title="Unauthorized" subtitle="Please Login"/>
+      )
+  }
+
+  const reservations = await getReservations({
+    authorId: currentUser.id,
+  })
+
+  if(reservations.length === 0) {
+    return (
+        <EmptyState title="No Reservations found" subtitle="Looks like you haven't reserved anything"/>
+    )
+  }
+
+  return (
+
+    <ReservationsClient reservations={reservations} currentUser={currentUser}/>
+  )
+  
+
+
+
+}
+export default ReservationsPage
--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -13,10 +13,12 @@ export const authOptions: AuthOptions = {
     GithubProvider({
       clientId: process.env.GITHUB_ID as string,
       clientSecret: process.env.GITHUB_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      allowDangerousEmailAccountLinking: true,
     }),
     CredentialsProvider({
       name: "credentials",
