26625fcfa642c19484aa281425c51449e9b60451
unknown <nikcevici13@gmail.com>
2024-04-29 16:40:32 +0200

    map

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { ToasterProvider } from "./providers/toaster-provider";
 import { LoginModal } from "./components/modals/login-modal";
 import getCurrentUser from "./actions/getCurrentUser";
 import { RentModal } from "./components/modals/RentModal";
+import SearchModal from "./components/modals/SearchModal";
 
 const font = Nunito({ subsets: ["latin"] });
 
@@ -25,6 +26,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={font.className}>
         <ToasterProvider />
+        <SearchModal />
         <RegisterModal />
         <LoginModal />
         <RentModal />
