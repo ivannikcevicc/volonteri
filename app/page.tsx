dad4972b625bf86ecdf6391c100cd13a14444c5f
unknown <nikcevici13@gmail.com>
2024-05-14 20:55:37 +0200

    another try

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import getCurrentUser from "./actions/getCurrentUser";
 import getJobs, { JobProps } from "./actions/getJobs";
 import { EmptyState } from "./components/EmptyState";
 import SearchHome from "./components/SearchHome";
-import { ListingCard } from "./components/listings/ListingCard";
+import { JobCard } from "./components/jobs/JobCard";
 import { Categories } from "./components/navbar/Categories";
 export const dynamic = "force-dynamic";
 
@@ -24,13 +24,9 @@ const Home = async ({ searchParams }: Props) => {
             <EmptyState showReset />
           ) : (
             <div className="pt-2 px-[5%] overflow-y-auto cat:h-[59vh] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8 ">
-              {jobs.map((listing: any) => {
+              {jobs.map((job) => {
                 return (
-                  <ListingCard
-                    key={listing.id}
-                    currentUser={currentUser}
-                    data={listing}
-                  />
+                  <JobCard key={job.id} currentUser={currentUser} data={job} />
                 );
               })}
             </div>
