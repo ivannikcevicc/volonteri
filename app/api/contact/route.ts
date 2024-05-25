0421531068dbb566633d3f18d664f774359e3418
unknown <nikcevici13@gmail.com>
2024-05-25 14:45:38 +0200

    aaa

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -1,19 +1,9 @@
 import { NextResponse } from "next/server";
 import getCurrentUser from "@/app/actions/getCurrentUser";
-import FormData from "form-data";
-import Mailgun from "mailgun.js";
+import { Resend } from "resend";
 
 export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
-
-  const mailgun = new Mailgun(FormData);
-  const mg = mailgun.client({
-    username: "brad@sandboxd36069e467e04cdaaa57a360478f6821.mailgun.org",
-    key:
-      process.env.MAILGUN_API_KEY ||
-      "c09a59d038bfaaab5173df5cd6dc384b-a2dd40a3-52dcc411",
-  });
-
   const body = await request.json();
 
   const { name, email, message } = body;
@@ -22,19 +12,18 @@ export async function POST(request: Request) {
     return NextResponse.error();
   }
 
-  mg.messages
-    .create("sandboxd36069e467e04cdaaa57a360478f6821.mailgun.org", {
-      from: `Excited User <${email}>`,
-      to: ["nikcevici13@gmail.com"],
-      subject: `Email Volonteri, ${name}!`,
-      text: `${message}`,
-    })
-    .then((msg: any) => NextResponse.json("msg: ", msg)) // logs response data
-    .catch((err: any) => NextResponse.json("err: ", err)); // logs any error
-
   if (!currentUser) {
     return NextResponse.error();
   }
+
+  const resend = new Resend(`${process.env.RESEND_API_KEY}`);
+
+  resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: "nikcevici13@gmail.com",
+    subject: `Message from ${name} - Volonteri`,
+    html: `<p>From: ${name}</p><p>Email: ${email}</p><br/><p>Message: ${message}</p>`,
+  });
 
   return NextResponse.json("aa");
 }
