9d3e43ba577456f2227c337a424ae415722bfa63
unknown <nikcevici13@gmail.com>
2024-05-25 14:47:42 +0200

    aaaaaaa

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -20,7 +20,7 @@ export async function POST(request: Request) {
 
   resend.emails.send({
     from: "onboarding@resend.dev",
-    to: "nikcevici13@gmail.com",
+    to: ["nikcevici13@gmail.com", "pejovic.nikolina2@gmail.com"],
     subject: `Message from ${name} - Volonteri`,
     html: `<p>From: ${name}</p><p>Email: ${email}</p><br/><p>Message: ${message}</p>`,
   });
